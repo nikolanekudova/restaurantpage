1911e1e404a9b6a3d946955d507d20dce09459ea
Nikola Nekudova <nikolka.nekudova@seznam.cz>
2022-10-20 21:01:28 +0200

    feat(): webpack repaired

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { renderHomepage } from "./renderHomepage";
 import { renderMenuPage } from "./renderMenuPage";
 import { renderContactPage } from "./renderContactPage";
+import "./style.css";
+import _ from 'lodash';
+import ContactSmaller from './contact-smaller.jpg';
+import FoodSmaller from './food-smaller.jpg';
+import BistroSmaller from './bistro-smaller.jpg';
 
 const buttonHome = document.getElementById("btn-home");
 const buttonOurMenu = document.getElementById("btn-our-menu");
